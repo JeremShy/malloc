88238257d63c04e15f01b3f964c1010a2fa4a30e
jcamhi <jeremshy@h3lp.fr>
2017-07-10 19:49:20 +0200

    tiny.c norme : Pas sur que ca marche !

--- tiny.c
+++ tiny.c
@@ -1,51 +1,51 @@
 #include <malloc.h>
 
-void	*tiny(size_t size, void *tiny[])
+static void	*create_page_ti(size_t *ancient_size, void *tiny[])
+{
+	void	*page;
+
+	page = create_new_page(tiny, get_t_psize());
+	if (!page)
+		return (NULL);
+	*ancient_size = get_t_psize() - sizeof(t_header);
+	return (page);
+}
+
+static void	create_header(void *page, size_t ancient_size, size_t size,
+	int header_creation)
+{
+	if (header_creation)
+	{
+		page = ((t_header*)page)->size + page + sizeof(t_header);
+		((t_header*)page)->size = ancient_size - sizeof(t_header) - size;
+		((t_header*)page)->used = 0;
+	}
+}
+
+void		*tiny(size_t size, void *tiny[])
 {
 	int		blocs_needed;
 	void	*page;
-	int		header_creation; // À 1 si il faut creer un header apres la nouvelle zone.
+	int		header_creation;
 	size_t	ancient_size;
 	void	*ret;
 
 	blocs_needed = sizeof(t_header) + size;
-	//printf("blocs needed : %d - %#x\n", blocs_needed, blocs_needed);
 	header_creation = 1;
 	if (!(page = find_space(tiny, blocs_needed, get_t_psize())))
 	{
-		//printf("Creating a new tiny page..\n");
-		page = create_new_page(tiny, get_t_psize());
-		if (!page)
-		{
-			// //printf("There was an error while creating the page");
+		if (!(page = create_page_ti(&ancient_size, tiny)))
 			return (NULL);
-		}
-		//printf("tiny page created at emplacement : %p\n", page);
-		ancient_size = get_t_psize() - sizeof(t_header);
-		//printf("ancient_size : %zu\n", ancient_size);
 	}
 	else
 	{
-		//printf("tiny zone found at emplacement : %p\n", page);
 		if (((t_header*)page)->size == blocs_needed + sizeof(t_header))
 			header_creation = 0;
-		else
-			ancient_size = ((t_header*)page)->size;
+		ancient_size = header_creation ? ((t_header*)page)->size : 0;
 	}
 	((t_header*)page)->size = size;
 	((t_header*)page)->used = 1;
-	//printf("size : %#lx\n", ((t_header*)page)->size);
-	//printf("page : %p\n", page);
-	//printf("debut (apres header): %p, fin : %p\n", page + sizeof(t_header), page + sizeof(t_header) + size);
 	ret = page + sizeof(t_header);
-	if (header_creation)
-	{
-		//printf("creating a new header after the new zone. (Ancient size : %#lx)\n", ancient_size);
-		page = page + sizeof(t_header) + ((t_header*)page)->size;
-		((t_header*)page)->size = ancient_size - sizeof(t_header) - size;
-		((t_header*)page)->used = 0;
-		//printf("New header size : %#zx (%zu bytes)\n", ((t_header*)page)->size, ((t_header*)page)->size);
-	}
-	//printf("returning alloc tiny %p\n", ret);
+	create_header(page, ancient_size, size, header_creation);
 	return (ret);
 }
--- utils.c
+++ utils.c
@@ -1,13 +1,13 @@
 #include <malloc.h>
 
-size_t get_t_psize(void) // Retourne la taille d'une page tiny.
+size_t get_t_psize(void)
 {
 	return ((100 * (sizeof(t_header) + TINY)) % getpagesize() == 0 ?
 		(100 * (sizeof(t_header) + TINY)) : ((((100 * (sizeof(t_header) + TINY))
 		/ getpagesize()) + 1) * getpagesize()));
 }
 
-size_t get_s_psize(void) // Retourne la taille d'une page small.
+size_t get_s_psize(void)
 {
 	return ((100 * (sizeof(t_header) + SMALL)) % getpagesize() == 0 ? \
 		(100 * (sizeof(t_header) + SMALL)) : ((((100 * (sizeof(t_header) + SMALL))
