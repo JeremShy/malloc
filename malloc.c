dd84ce46cbb92a281eccb5f5d4c7a8dbd34451b5
jcamhi <jeremshy@h3lp.fr>
2017-07-10 19:33:00 +0200

    Debut de la norme

--- malloc.c
+++ malloc.c
@@ -8,15 +8,11 @@ void	free(void *ptr)
 	int				index;
 	int 		new_size;
 
-	//printf ("////////////APPEL A FREE//////////////\n");
-	// show_alloc_mem();
-	// if (!ptr)
+	if (!ptr)
 		return ;
-	// //printf("calling free with ptr = %p\n", ptr);
 	new_size = 0;
 	if ((header = find_header_tiny_or_small(ptr, &g_data, &new_size)))
 	{
-		//printf("malloc found ! at emplacement : %p\n", (void*)header + sizeof(t_header));
 		if (new_size == -1)
 		{
 			return;
@@ -27,40 +23,18 @@ void	free(void *ptr)
 	}
 	else if ((index = find_header_large(ptr, g_data.large)) != -1)
 	{
-		// //printf("large malloc found.\n");
-		if (!unmap_and_shift_page(index, g_data.large))
-		{
-			// //printf("unmap failed.\n");
-			// perror("");
-		}
-	}
-	else
-	{
-		//printf("malloc not found.\n");
+		unmap_and_shift_page(index, g_data.large);
 	}
 }
 
 void	*malloc(size_t size)
 {
-	// show_alloc_mem();
-	// write (1, "////////////APPEL A MALLOC//////////////\n", sizeof("////////////APPEL A MALLOC//////////////\n"));
-	// write(1, "a\n", 2);
-	// //printf("size : %zu\n", size);
 	if (size <= TINY)
-	{
-		// write(1, "1\n", 2);
 		return (tiny(size, g_data.tiny));
-	}
 	else if (size <= SMALL)
-	{
-		// write(1, "2\n", 2);
 		return (small(size, g_data.small));
-	}
 	else
-	{
- 	// 	write(1, "3\n", 2);
 		return (large(size, g_data.large));
-	}
 	return (NULL);
 }
 
