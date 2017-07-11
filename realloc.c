6beefc263599a21aede56fe48211d3e51b28c4a1
jcamhi <jeremshy@h3lp.fr>
2017-07-11 21:19:04 +0200

    Rainbow Dash is best pony !

--- realloc.c
+++ realloc.c
@@ -1,10 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   realloc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcamhi <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/11 21:17:34 by jcamhi            #+#    #+#             */
+/*   Updated: 2017/07/11 21:17:35 by jcamhi           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <malloc.h>
 
-void	*realloc_large(void *header, size_t possible_size, t_data *g_data,
+static void	*shrink_allocation_lg(void *header, size_t size, size_t old_size,
+	void *ptr)
+{
+	void	*next_header;
+
+	((t_header*)header)->size = size;
+	next_header = (char*)header + sizeof(t_header)
+		+ ((t_header*)header)->size;
+	((t_header*)next_header)->used = 0;
+	((t_header*)next_header)->size = old_size - size - sizeof(t_header);
+	return (ptr);
+}
+
+void		*realloc_large(void *header, size_t possible_size, t_data *g_data,
 	size_t size)
 {
 	size_t	old_size;
-	void	*next_header;
 	void	*ptr_2;
 	void	*ptr;
 
@@ -12,13 +36,7 @@ void	*realloc_large(void *header, size_t possible_size, t_data *g_data,
 	ptr = (char*)header + sizeof(t_header);
 	old_size = ((t_header*)header)->size;
 	if (size + sizeof(t_header) < (size_t)old_size)
-	{
-		((t_header*)header)->size = size;
-		next_header = (char*)header + sizeof(t_header) + ((t_header*)header)->size;
-		((t_header*)next_header)->used = 0;
-		((t_header*)next_header)->size = old_size - size - sizeof(t_header);
-		return (ptr);
-	}
+		return (shrink_allocation_lg(header, size, old_size, ptr));
 	else
 	{
 		ptr_2 = malloc(size);
@@ -31,7 +49,7 @@ void	*realloc_large(void *header, size_t possible_size, t_data *g_data,
 	}
 }
 
-void	*do_realloc(void *ptr, size_t size, t_data *g_data)
+void		*do_realloc(void *ptr, size_t size, t_data *g_data)
 {
 	void	*header;
 	size_t	possible_size;
